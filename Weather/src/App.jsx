2d6034534db836b164b51426887b6b5f75ae801c
Sidd <siddhantd37@gmail.com>
2024-05-27 03:17:16 +0530

    final

--- Weather/src/App.jsx
+++ Weather/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Weather from './Weather'
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <div>
+      <Weather/>
+    </div>
   )
 }
 
 export default App
+
